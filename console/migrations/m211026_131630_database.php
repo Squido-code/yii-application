c25bf8e7054643ee350af82af7dc84440244507f
Squido <squidocode@hotmail.com>
2021-10-26 15:17:40 +0200

    migration

---
+++ console/migrations/m211026_131630_database.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m211026_131630_database
+ */
+class m211026_131630_database extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m211026_131630_database cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m211026_131630_database cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
