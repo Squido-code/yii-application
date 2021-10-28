b844f728faf14886cfa5f9c8605add32bac59032
Squido <squidocode@hotmail.com>
2021-10-28 18:31:49 +0200

    moving web to root

---
+++ js/checkout.js
@@ -0,0 +1,76 @@
+const stripe = Stripe('pk_test_51JhYE9EsSA3dVfnSOQeS26tCRHT8D7U00cIi9LolSbdX9KV7ejQUz7TyPdpVWHjPK5i7G5EhCYZXzbFJpdRYs3kS00r8HgIT9O');
+const donacion = document.querySelector('#donacion')
+const bronze = document.querySelector('#bronze')
+const silver = document.querySelector('#silver')
+const gold = document.querySelector('#gold')
+
+
+donacion.addEventListener('click', () => {
+    fetch('/stripe/donacion', {
+        method: 'POST',
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (session) {
+            return stripe.redirectToCheckout({sessionId: session.id});
+        })
+        .then(function (result) {
+            if (result.error) {
+                alert(result.error.message);
+            }
+        });
+})
+
+bronze.addEventListener('click', () => {
+    fetch('/stripe/checkout?subscription=1', {
+        method: 'POST',
+        data: {_csrf: csrfToken},
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (session) {
+            return stripe.redirectToCheckout({sessionId: session.id});
+        })
+        .then(function (result) {
+            if (result.error) {
+                alert(result.error.message);
+            }
+        });
+})
+
+silver.addEventListener('click', () => {
+    fetch('/stripe/checkout?subscription=2', {
+        method: 'POST',
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (session) {
+            return stripe.redirectToCheckout({sessionId: session.id});
+        })
+        .then(function (result) {
+            if (result.error) {
+                alert(result.error.message);
+            }
+        });
+})
+
+
+gold.addEventListener('click', () => {
+    fetch('/stripe/checkout?subscription=3', {
+        method: 'POST',
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (session) {
+            return stripe.redirectToCheckout({sessionId: session.id});
+        })
+        .then(function (result) {
+            if (result.error) {
+                alert(result.error.message);
+            }
+        });
+})
