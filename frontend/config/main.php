b02be0a2dc6854700c10065ac58b583e68c52fcb
squidoCode <squidocode@hotmail.com>
2021-11-04 17:01:18 +0100

    log updated

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -62,10 +62,10 @@ return [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'flushInterval' => 100,
             'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning', 'trace'],
-                ],
+//                [
+//                    'class' => 'yii\log\FileTarget',
+//                    'levels' => ['error', 'warning', 'trace'],
+//                ],
                 [
                     'class' => 'yii\log\FileTarget',
                     'levels' => ['error', 'warning', 'info'],
