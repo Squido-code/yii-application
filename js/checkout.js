061ce12ac77223aa1aab6bc105750ad5218ac300
squidoCode <squidocode@hotmail.com>
2021-11-27 17:56:23 +0100

    limpieza main index

--- js/checkout.js
+++ js/checkout.js
@@ -5,23 +5,6 @@ const silver = document.querySelector('#silver')
 const gold = document.querySelector('#gold')
 
 
-donacion.addEventListener('click', () => {
-    fetch('/stripe/donacion', {
-        method: 'POST',
-    })
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (session) {
-            return stripe.redirectToCheckout({sessionId: session.id});
-        })
-        .then(function (result) {
-            if (result.error) {
-                alert(result.error.message);
-            }
-        });
-})
-
 bronze.addEventListener('click', () => {
     alert("boton funciona")
     fetch('/stripe/checkout?subscription=1', {
