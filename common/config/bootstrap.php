61cd72e7975a069ef02d5f095fe9ee1bbdeb3f35
squidoCode <squidocode@hotmail.com>
2021-11-27 18:33:04 +0100

    permisos actualizados

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,5 +3,5 @@ Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
-yii::setAlias('@web', dirname(dirname(__DIR__)) . '/frontend/web');
+//yii::setAlias('@web', dirname(dirname(__DIR__)) . '/frontend/web');
 
